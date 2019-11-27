1b39a9799b34dfcc68428642f6d6d520e9c02a1a
Daniel Taquez <daniel.taquez@du.edu>
2019-11-26 23:57:26 -0700

    landingPage/styling

--- dan-portfolio/src/App.js
+++ dan-portfolio/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
+        <Header className=""  className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -26,6 +26,7 @@ class App extends Component {
             </Navigation>
         </Drawer>
         <Content>
+
             <div className="page-content" />
             <Main/>
         </Content>
--- dan-portfolio/src/components/landingpage.js
+++ dan-portfolio/src/components/landingpage.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class Landing extends Component {
     render () {
         return(
-            <div><h1>Landing page</h1></div>
-        )
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col= {12}>
+                        <img
+                        src= "https://images.pexels.com/photos/2315712/pexels-photo-2315712.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                        alt="avatar"
+                        ClassName= "avatar-img"
+                        />
+                        <div className="banner-text">
+                            <h1>Full Stack Web Dev.</h1>
+
+                            <hr/>
+
+                            <p>HTML/CSS |Semantic UI | JavaScript | React | Node.js | Express | MongoDB </p>
+
+                        </div>
+                    </Cell>
+                </Grid> 
+            </div>
+         )
     }
 };
 
