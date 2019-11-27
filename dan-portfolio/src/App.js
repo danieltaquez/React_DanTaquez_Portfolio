3fe8c8f1dd3a0b24bf2ac8650c924c3b6dbfa403
Daniel Taquez <daniel.taquez@du.edu>
2019-11-26 22:02:37 -0700

    nav bar issues explored/resp issue

--- dan-portfolio/src/App.js
+++ dan-portfolio/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl'
-import Main from './components/main'
-import {Link} from 'react-router-dom'
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Main from './components/main';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
+class App extends Component {
+  render() {
+    return (
       <div className="demo-big-content">
     <Layout>
-        <Header title="Title" scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">MyPortfolio</Link>} scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -16,7 +17,7 @@ function App() {
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="Title">
+        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">MyPortfolio</Link>}>
             <Navigation>
               <Link to="/resume">Resume</Link>
               <Link to="/aboutme">About Me</Link>
@@ -25,13 +26,14 @@ function App() {
             </Navigation>
         </Drawer>
         <Content>
-
-            <div className="page-content"/>
+            <div className="page-content" />
             <Main/>
         </Content>
     </Layout>
 </div>
-  );
+
+    );
+  }
 }
 
 export default App;
