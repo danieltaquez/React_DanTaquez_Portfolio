bac72f5b50baea8c78b22331a42c57148f970727
Daniel Taquez <daniel.taquez@du.edu>
2019-12-01 21:56:05 -0700

    tabContent/porjects

--- dan-portfolio/src/components/projects.js
+++ dan-portfolio/src/components/projects.js
@@ -1,9 +1,49 @@
 import React, {Component} from 'react';
+import {Tabs,Tab} from 'react-mdl'
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {activeTab: 0};
+    }
+    
+    toggleCategories() {
+
+        if(this.state.activeTab === 0){
+        return(
+            <div><h1>This is React</h1></div>
+          )
+      } else if(this.state.activeTab === 1){
+        return (
+            <div><h1>This is Python</h1></div>
+          )
+      } else if(this.state.activeTab === 2){
+        return (
+            <div><h1>This is Javascript</h1></div>
+          )
+      } else if(this.state.activeTab === 3){
+        return (
+            <div><h1>This is MongoDB</h1></div>
+          )
+    }
+};
+
+    
+
     render () {
         return(
-            <div><h1>Projects page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Python</Tab>
+                    <Tab>Javascript</Tab>
+                    <Tab>Node.js</Tab>
+                </Tabs>
+
+                <section className="projects-grid">
+                    {this.toggleCategories()}
+                </section>
+            </div>
         )
     }
 };
